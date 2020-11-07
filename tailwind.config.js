f55787baa4249991d65ee8986d9090ab26635d5e
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-07 22:47:59 +0100

    add use the actual file: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        "ofn-orange": "#E4B231",
+        "ofn-green": "#69A95D"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
