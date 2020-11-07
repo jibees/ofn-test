f0664c818763413854b700998db77e250b31ea8b
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-07 22:35:02 +0100

    add tailwind

---
+++ .storybook/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/assets/main.css';
+
+const Layout = ({ children }) => {
+  return (
+      <div>
+      {children}
+      </div>
+  )
+}
+
+export default Layout;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import Layout from './Layout';
+
+addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
