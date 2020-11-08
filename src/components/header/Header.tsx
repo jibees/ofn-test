fce41271a332834d5c887648a73444aa3309a57f
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-08 09:18:58 +0100

    add <Header /> component and its stories

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ReactComponent as Logo }from "./OFN_logo.svg";
+import { ReactComponent as Cart }from "./shopping-cart.svg";
+export interface HeaderProps {
+    /**
+     * The number of the product
+     */
+    itemsCount: number,
+}
+
+/**
+ * Header for the APp
+ */
+export const Header: React.FC<HeaderProps> = ({
+    itemsCount,
+ ...props
+}) => {
+  return (
+    <nav className=" bg-white p-6 shadow-lg">
+        <div className="flex items-center justify-between flex-wrap m-auto max-w-screen-lg">
+            <div className="flex items-center flex-no-shrink text-gray-800 mr-6">
+                <Logo />
+                <span className="text-gray-700 text-lg tracking-tight ml-4">Open Food<br />Network</span>
+            </div>
+            <div className="flex-grow flex items-center w-auto">
+                <div className="text-sm flex-grow">
+                    
+                </div>
+                <div className="text-gray-700 flex items-center">
+                    <Cart className="fill-current" />
+                    <div className="ml-4 text-base">My Cart</div>
+                    {itemsCount !== 0 && <div className="text-base ml-1">({itemsCount})</div>}
+                </div>
+            </div>
+        </div>
+    </nav>
+  );
+};
