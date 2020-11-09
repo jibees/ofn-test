74fb31853213f7003b2821daf34c4e9aa36a844c
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-09 13:33:20 +0100

    add the possibility to remove product from My Cart

--- src/containers/SelectedProductList.tsx
+++ src/containers/SelectedProductList.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { ProductList as ProductListComponent } from "../components/product/ProductList";
-import { selectedProductList } from "../state/selectedProducts";
+import {
+  selectedProductList,
+  selectedProductsIdState,
+} from "../state/selectedProducts";
 import { fromApiToComponentMapper } from "./ProductList";
 
 export function SelectedProductList() {
   const productList = useRecoilValue(selectedProductList);
-  const productListForComponent = productList.map(fromApiToComponentMapper);
+  const setSelectedProduct = useSetRecoilState(selectedProductsIdState);
+  const productListForComponent = productList
+    .map(fromApiToComponentMapper)
+    .map((p) => ({
+      ...p,
+      inMyCart: true,
+      // For each product, add its own handler to remove the current product from 'My Cart'
+      removeHandler: (id: number) => {
+        // Remove the current product in the oldList of selectedProductsId
+        setSelectedProduct((oldList) => {
+          return oldList.filter((item) => item !== id);
+        });
+      },
+    }));
   return <ProductListComponent products={productListForComponent} />;
 }
