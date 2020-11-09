a6a6c650e07e090d9d14634eaa92d9792f26640a
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-09 12:04:36 +0100

    add handler on 'My Cart' label for header

--- src/components/header/Header.stories.tsx
+++ src/components/header/Header.stories.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import { Header, HeaderProps } from './Header';
+import { Header, HeaderProps } from "./Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
-  
+  argTypes: { myCartHandler: { action: "'My Cart' clicked" } },
 } as Meta;
 
 const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  itemsCount: 0
+  itemsCount: 0,
 };
 
 export const WithProducts = Template.bind({});
 WithProducts.args = {
-  itemsCount: 2
+  itemsCount: 2,
 };
