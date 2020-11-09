849ad2fa14010e074d539044db1cec44a8373db0
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2020-11-09 09:24:47 +0100

    add brand new global state manager: recoil <3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './assets/main.css'
 import { Header } from './components/header/Header';
+import { RecoilRoot } from 'recoil';
+import { ProductList } from './components/product/ProductList';
 
 function App() {
   return (
-    <div >
-      <Header itemsCount={0} />
-      <div className="w-full">
-
+    <RecoilRoot>
+      <div >
+        <Header itemsCount={0} />
+        <div className="w-full">
+          
+        </div>
       </div>
-    </div>
+    </RecoilRoot>
   );
 }
 
